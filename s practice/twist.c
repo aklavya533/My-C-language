6de3eee579ad2d7020de6126d8393f312f99a202
Aklavya <ashminz0511@gmail.com>
2025-08-07 00:12:37 +0530

    Progress

--- s practice/twist.c
+++ s practice/twist.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
 int main(){
-    int a , b , ;
+    int a , b , sum = 0, subtraction = 0, multiplication = 0;
+    float division;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+    sum = a + b;
+    subtraction = a - b;
+    multiplication = a * b;
+    division =  a / b;
+    printf("The sum of two numbers is %d\n", sum);
+    printf("The subtraction of two numbers is %d\n", subtraction);
+    printf("The multiplication of two numbers is %d\n", multiplication);
+    printf("The division of two numbers is %2f\n", division);
+    return 0;
 }
