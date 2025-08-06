b1c9624582981a7b875f85fb2f651b6b9a94561a
Aklavya <ashminz0511@gmail.com>
2025-08-06 23:57:38 +0530

    Update sum.c

--- s practice/sum.c
+++ s practice/sum.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
 int main(){
-    
+    int a,b,sum=0;
+    printf("Enter two numbers");
+    scanf("%d%d",&a,&b);
+    sum = a + b;
+    printf("The sum of two numbers is %d\n",sum);
+    return 0;
 }
